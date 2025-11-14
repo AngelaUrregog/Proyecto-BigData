37d73f23279e6f2538019c874f09165cfd8b7ceb
Angela Urrego Gomez <angelamugomez@hotmail.com>
2025-11-13 20:40:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from dotenv import load_dotenv
 import os
-from Helpers import mongoDB, ElasticSearch, funciones
+from Helpers import mongoDB, Elasticsearch, funciones
 
 # Cargar variables de entorno
 load_dotenv()
